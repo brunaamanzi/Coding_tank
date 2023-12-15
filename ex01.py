1942067bd527c8b08bf2a34e946f9485a549b4bc
brunamanzi <brunaamanzi@gmail.com>
2023-12-15 19:28:46 -0300

    Initial commit

---
+++ ex01.py
@@ -0,0 +1,12 @@
+"""1. Considere o seguinte trecho de código, parte do preenchimento do perfil de investidor do banco:
+
+i = input('Você sabe o que é uma RENDA FIXA?: ')
+Caso o usuário digite na caixa de texto de entrada "True", qual será o tipo de dado da variável i?
+
+a) int
+b) str
+c) float
+d) bool
+e) list
+"""
+# O tipo de variável será: b) str, pois quando não especificamos a formatação, o python assume que é uma string por padrão.
---
+++ ex02.py
@@ -0,0 +1,25 @@
+"""2. Você precisa implementar um código em Python que solicite ao usuário o valor principal (capital) inicial, a taxa de juros por período e o tempo em que o dinheiro é emprestado ou investido. Com base nesses valores, o programa deve calcular os juros simples e o montante total ao final do período. Assuma que temos apenas um único mês de investimento/empréstimo. Assinale a alternativa que corretamente implementa este procedimento:
+
+a) juros = principal *= taxa
+montante = principal + juros
+
+b) juros = principal * taxa
+montante = principal += juros
+
+c) juros = principal *= taxa
+montante = principal += juros
+
+d) juros = principal + taxa
+montante == principal ** juros
+
+e) juros = principal * taxa
+montante = principal + juros
+
+Dica: O montante é calculado somando o principal ao juros e os juros são o produto do principal pela taxa."""
+capital_inicial = float(input('Insira o Capital Inicial: R$'))
+taxa_juros = float(input('Insira a taxa de juros no formato 0.0 a 1.0, sendo 0,0 = 0% e 1,0 = 100%: '))
+tempo = int(input('Insira o número de meses em que o dinheiro ficará aplicado: '))
+juros_simples = taxa_juros * capital_inicial
+montante = capital_inicial+(capital_inicial*taxa_juros*tempo)
+print(f'O valor referente aos juros simples a cada mês é de: R${juros_simples}')
+print(f'O montante final é de R${montante}')
